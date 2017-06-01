47d972d7b77fa215d7aae86841d1b7fdc019ff52
nmares <nmares@hawk.iit.edu>
2017-05-31 23:44:27 -0500

    adding 99 bottles of beer song

---
+++ pine/chap-07/a_few_things3/99bottles.rb
@@ -0,0 +1,11 @@
+
+input1 =99
+index = input1
+while index != 0
+  puts '' + index.to_s + ' bottles of beer on the wall!'
+  puts '' + index.to_s + ' bottles of beer!'
+  puts 'Ya take one down, pass it around!'
+index = index - 1
+  puts '' + index.to_s + ' bottles of beer on the wall!'
+  puts ''
+end
