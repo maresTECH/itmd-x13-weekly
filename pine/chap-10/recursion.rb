12a1702241ecfd65d5350058fc77283624d9114a
nmares <nmares@hawk.iit.edu>
2017-06-01 15:41:24 -0500

    recusion example

---
+++ pine/chap-10/recursion.rb
@@ -0,0 +1,16 @@
+#classic recursion looping effect
+def ask_recursively question
+  puts question
+  reply = gets.chomp.downcase
+
+  if reply == 'yes'
+    true
+  elsif reply == 'no'
+    false
+  else
+    puts 'Please answer "yes" or "no".'
+    ask_recursively question # This is where the magic happes
+  end
+end
+
+ask_recursively 'Do you wet the bed?'
