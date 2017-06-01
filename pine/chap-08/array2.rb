df64690f25c06d937452059240c6790c357d9b30
nmares <nmares@hawk.iit.edu>
2017-06-01 00:25:52 -0500

    adding more array methods..

---
+++ pine/chap-08/array2.rb
@@ -0,0 +1,14 @@
+#more array methods
+
+foods = ['artichoke', 'brioche', 'caramel']
+
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods .join(', ')
+puts
+puts foods.join(' :) ') + ' 8)'
+200.times do
+  puts []
+end
