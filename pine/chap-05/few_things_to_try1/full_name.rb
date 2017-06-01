148839bd0de86fbb1f636f0babbc286bf407f377
nmares <nmares@hawk.iit.edu>
2017-05-31 21:08:15 -0500

    adding Few things

---
+++ pine/chap-05/few_things_to_try1/full_name.rb
@@ -0,0 +1,12 @@
+puts''
+puts 'Greetings! What is your first name?'
+fname = gets.chomp
+puts '..and your middle name is?'
+mname = gets.chomp
+puts '..and your last name?'
+lname = gets.chomp
+puts ''
+puts 'Nice to finally meet you, ' + fname + '!'
+puts ''
+puts 'Your full name is ' + fname + ' ' + mname + ' ' + lname + '.'
+puts ''
