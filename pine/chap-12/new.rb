31f71676e9aaebdca53554c3177400fbb4bb4eb5
nmares <nmares@hawk.iit.edu>
2017-06-02 01:35:16 -0500

    new classes

---
+++ pine/chap-12/new.rb
@@ -0,0 +1,7 @@
+alpha = Array.new + [12345] #array addition
+beta = String.new + 'hello' #String addition
+karma = Time.new # current date and Time
+
+puts "alpha = #{alpha}"
+puts "beta = #{beta}"
+puts "karma = #{karma}"
