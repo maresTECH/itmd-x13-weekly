27fae0dc549566c8174978fc3f17b9359e15beab
nmares <nmares@hawk.iit.edu>
2017-06-02 02:05:44 -0500

    Super Strings

---
+++ pine/chap-12/super_strings.rb
@@ -0,0 +1,11 @@
+puts "Hello. My name is Julian."
+puts "I'm extremly perceptive."
+puts "Whats your name?"
+
+name = gets.chomp
+puts "Hi, #{name}."
+
+if name[0] == 'N'
+  puts 'You have excellant taste in footware.'
+  puts 'I can just tell.'
+end
