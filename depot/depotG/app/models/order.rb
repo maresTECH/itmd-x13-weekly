3e298ec14bc210d92bc5c4bbfc75ff3f6ee9674c
nmares <nmares@hawk.iit.edu>
2017-06-20 01:57:33 -0500

    add more ajax

--- depot/depotG/app/models/order.rb
+++ depot/depotG/app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
+
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
@@ -9,7 +9,6 @@ class Order < ApplicationRecord
       line_items << item
     end
   end
-end
 
   enum pay_type: {
     "Check" => 0,
