5518d5f6d5a8660ef4bc29eeb1680cf9a871c215
nmares <nmares@hawk.iit.edu>
2017-06-20 01:34:01 -0500

    add payment with validation

--- depot/depotG/app/models/order.rb
+++ depot/depotG/app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+end
+
   enum pay_type: {
     "Check" => 0,
     "Credit Card" => 1,
